fd92c240b4163ee565230a4d4c777e64b9aeb28d
jtn0123 <jtn0123@gmail.com>
2025-08-07 18:08:20 -0700

    chore: save current debug improvements (crash persist, log endpoints, safety)

--- src/version.rs
+++ src/version.rs
@@ -2,7 +2,7 @@
 
 // Display version - this is what users see on screen
 // Update this when making significant changes
-pub const DISPLAY_VERSION: &str = "v5.93";
+pub const DISPLAY_VERSION: &str = "v6.00";
 
 // Cargo package version from Cargo.toml
 pub const CARGO_VERSION: &str = env!("CARGO_PKG_VERSION");
